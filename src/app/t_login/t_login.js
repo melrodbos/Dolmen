697d73eaa9992c56f21277d2ed100051fd2e06e2
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-07-16 22:27:38 -0400

    commenting out futile attempts at form validation...

--- src/app/t_login/t_login.js
+++ src/app/t_login/t_login.js
@@ -1,7 +1,7 @@
-angular.module('app', [])
-  .controller('Controller', function($scope) {
-    $scope.savedUser = null;
-    $scope.save = function(user) {
-      $scope.savedUser = angular.copy(user);
-    };
-  });
+// angular.module('app', [])
+//   .controller('Controller', function($scope) {
+//     $scope.savedUser = null;
+//     $scope.save = function(user) {
+//       $scope.savedUser = angular.copy(user);
+//     };
+//   });
