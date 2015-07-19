e5d0ab959e72ee084fc14c20eb1edd26ebee2d53
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-07-19 13:30:23 -0400

    still working on displaying the requests after submitting

--- src/app/po_dash/show_request.js
+++ src/app/po_dash/show_request.js
@@ -7,7 +7,12 @@
       app.controller('ShowController', function($http) {
 
         var show = this;
-        show.requests = [];
+        this.request = { };
+
+        this.addRequest = function(info) {
+          info.requests.push(this.request);
+          this.request = { };
+        };
 
         $http.get('https://dolmen.firebaseio.com/')
         .then(function(response){
