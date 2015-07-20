8c12e171a96a8c56da448970cbef9cd2c1e9f91d
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-07-20 11:07:22 -0400

    added phone and address fields to html and js

--- src/app/po_dash/po_dash.js
+++ src/app/po_dash/po_dash.js
@@ -15,11 +15,15 @@
         self.submit = function() {
           self.data.$add({
             title: self.title,
+            address: self.address,
+            phone: self.phone,
             description: self.description,
             instructions: self.instructions
           });
           // the following will clear the fields once the request is submited:
           self.title = '';
+          self.address = '';
+          self.phone = '';
           self.description = '';
           self.instructions = '';
         };
