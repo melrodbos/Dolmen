2e74aa376d17afb50973a316ee47d05aedefbaa0
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-07-21 08:50:11 -0400

    google login setup done, firebase simple email and password removed

--- src/app/index.sevices.js
+++ src/app/index.sevices.js
@@ -2,40 +2,53 @@
 ;(function(){
   'use strict';
 
-  var services = angular.module('dolmen.services', ['firebase']);
+  var ref = new Firebase("https://dolmen.firebaseio.com");
+ref.authWithOAuthPopup("google", function(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Authenticated successfully with payload:", authData);
+  }
+});
 
-  services.factory ('authServices', [
-    '$state',
-    '$timeout',
-    '$firebaseAuth',
 
-    function( $state, $timeout, $firebaseAuth )
-    {
-      var authServices = {};
-      var auth = new Firebase( 'https://dolmen.firebaseio.com');
-      authServices.authObj = $firebaseAuth(auth);
-      console.log(authServices.authObj);
-      authServices.login = function (user, pass) {
-        authServices.authObj.$authWithPassword({
-          email : user,
-          password :pass
 
-        })
-        .then(function(response){
-          $state.go('po_dash');
-        });
-      };
 
-    //   authServices.logout = function () {
-    //     authServices.authObj.$unauth();
-    //   };
-    //   authServices.getloginstatus = function() {
-    //   //   //anytime I call a fn in firebase I use $getAuth...
-    //   return authServices.authObj.$getAuth();
-    // };
+//Firebase simple email and password login
+  // var services = angular.module('dolmen.services', ['firebase']);
+  //
+  // services.factory ('authServices', [
+  //   '$state',
+  //   '$timeout',
+  //   '$firebaseAuth',
+  //
+  //   function( $state, $timeout, $firebaseAuth )
+  //   {
+  //     var authServices = {};
+  //     var auth = new Firebase( 'https://dolmen.firebaseio.com');
+  //     authServices.authObj = $firebaseAuth(auth);
+  //     console.log(authServices.authObj);
+  //     authServices.login = function (user, pass) {
+  //       authServices.authObj.$authWithPassword({
+  //         email : user,
+  //         password :pass
+  //
+  //       })
+  //       .then(function(response){
+  //         $state.go('po_dash');
+  //       });
+  //     };
+  //
+  //   //   authServices.logout = function () {
+  //   //     authServices.authObj.$unauth();
+  //   //   };
+  //   //   authServices.getloginstatus = function() {
+  //   //   //   //anytime I call a fn in firebase I use $getAuth...
+  //   //   return authServices.authObj.$getAuth();
+  //   // };
+  //
+  //     return authServices;
+  //   }
 
-      return authServices;
-    }
-
-  ]);
+  // ]);
 })();
