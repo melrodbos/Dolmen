5015b3e945687a533cb44e28f1d8b1edeb639694
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-07-22 16:48:24 -0400

    commented out service factory for future use...

--- src/app/index.sevices.js
+++ src/app/index.sevices.js
@@ -1,31 +1,27 @@
 /* global angular Firebase */
-;(function(){
-  'use strict';
+// ;(function(){
+//   'use strict';
+//
+//   var services = angular.module( 'dolmen.services', ['firebase']);
 
-  var services = angular.module( 'dolmen.services', ['firebase']);
+  // services.factory( 'authServices', function( $state, $firebaseAuth ){
+    // FIXME: Move all of this code into `pologin.controller.js:PoLoginController`
+      // authServices = {};
+      //
+      // var ref = new Firebase( 'https://dolmen.firebaseio.com' );
+      // authServices.authObj = $firebaseAuth( ref );
+      //
+      // authServices.login = function()
+      // {
+      //   authServices.authObj.$authWithOAuthPopup( 'google')
+      //     .then(function(ref){
+      //       $state.go('po_dash');
+      //     })
+      //     .catch( function( err ){
+      //       // console.log( err );
+      //     });
+      // };
+  //   }
+  // );
 
-  services.factory( 'authServices', [
-    '$state',
-    '$timeout',
-    '$firebaseAuth',
-    function( $state, $timeout, $firebaseAuth )
-    {
-      authServices = {};
-
-      var ref = new Firebase( 'https://dolmen.firebaseio.com' );
-      authServices.authObj = $firebaseAuth( ref );
-
-      authServices.login = function()
-      {
-        authServices.authObj.$authWithOAuthPopup( 'google')
-          .then(function(ref){
-            $state.go('po_dash');
-          })
-          .catch( function( err ){
-            // console.log( err );
-          });
-      };
-    }
-  ]);
-
-})();
+// })();
