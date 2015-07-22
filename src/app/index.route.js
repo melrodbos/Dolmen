287a88055ee6fc844d6518ca218c3b362ca0915f
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-07-22 07:42:11 -0400

    removing unused routes

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -42,50 +42,26 @@
       templateUrl: 'app/features/features.html'
     })
 
-    .state('login', {
-      url: '/login',
-      templateUrl: 'app/login/login.html'
-    })
-
     .state('po_login', {
       url: '/login',
       templateUrl: 'app/po_login/po_login.html',
       controller: 'PoLoginController',
       controllerAs: 'PoLogin',
-      // data: {
-      //   needsAuth: false
-      // }
-    })
-    //Don't need auth to go to this route:
-    .state('t_login', {
-      url: '/t_login',
-      templateUrl: 'app/t_login/t_login.html',
-      // controller: 'TLoginController',
-      // controllerAs: 'tLogin',
-      // data: {
-      //   needsAuth: false
-      // }
+      data: {
+        needsAuth: false
+      }
     })
 
     .state('po_dash', {
       url: '/dashboard',
       templateUrl: 'app/po_dash/po_dash.html',
       //Since I need authorization to go to this route, it needs to evaluate to true.
-      // controller: 'ShowController',
-      // controllerAs: 'showM',
-      // data: {
-      //   needsAuth: true
-      // }
+      controller: 'ShowController',
+      controllerAs: 'showM',
+      data: {
+        needsAuth: true
+      }
     })
-
-    .state('t_dash', {
-      url: '/t_dash',
-      templateUrl: 'app/t_dash/t_dash.html',
-      //Totally need authorization to get here...
-      // data: {
-      //   needsAuth: true
-      // }
-    });
 }//End of routeConfig
 
 })();
