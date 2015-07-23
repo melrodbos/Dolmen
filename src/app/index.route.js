fa0f1fbe45454484c0d19527c31c5c1983102e1e
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-07-23 10:32:21 -0400

    created route for maintenance request form

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -1,3 +1,4 @@
+/* global angular */
 (function() {
   'use strict';
 
@@ -5,26 +6,27 @@
   .config(routeConfig);
 
   /** @ngInject */
-  function routeConfig(
-  $urlRouterProvider,
-  $stateProvider) {
+  function routeConfig($urlRouterProvider, $stateProvider) {
   $urlRouterProvider.otherwise('/');
+
   $stateProvider
     .state('home', {
       url: '/',
       templateUrl: 'app/main/main.html',
       controller: 'MainController',
-      controllerAs: 'main',
+      controllerAs: 'main'
     })
 
     .state('main', {
       url: '/',
-      templateUrl: 'app/main/main.html'
+      templateUrl: 'app/main/main.html',
+      controller: 'PoLoginController',
+      controllerAs: 'PoLogin',
     })
 
     .state('features', {
       url: '/features',
-      templateUrl: 'app/features/features.html'
+      templateUrl: 'app/features/features.html',
     })
 
     .state('po_login', {
@@ -32,9 +34,6 @@
       templateUrl: 'app/po_login/po_login.html',
       controller: 'PoLoginController',
       controllerAs: 'PoLogin',
-      data: {
-        needsAuth: false
-      }
     })
 
     .state('po_dash', {
@@ -43,20 +42,13 @@
       //Since I need authorization to go to this route, it needs to evaluate to true.
       controller: 'ShowController',
       controllerAs: 'showM',
-      data: {
-        needsAuth: true
-      }
     })
 
     .state('po_form', {
-      url: '/maintenance_request',
-      templateUrl: 'app/po_dash/po_dash.html',
-      //Since I need authorization to go to this route, it needs to evaluate to true.
-      controller: 'ShowController',
-      controllerAs: 'showM',
-      data: {
-        needsAuth: true
-      }
+      url: '/requests',
+      templateUrl: 'app/po_dash/po_form.html',
+      controller: 'RequestController',
+      controllerAs: 'request',
     });
 
 }//End of routeConfig
