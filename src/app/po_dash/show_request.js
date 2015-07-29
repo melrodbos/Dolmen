1a06cd7f02de91ae4abc653e14d05ccfe0e25aa8
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-07-29 14:58:07 -0400

    beautifying code

--- src/app/po_dash/show_request.js
+++ src/app/po_dash/show_request.js
@@ -3,26 +3,21 @@
   'use strict';
     var app = angular.module('dolmen');
 
-      app.controller('ShowController', function($http) {
+      app.controller('ShowController', function( $http, $scope ) {
 
         var show = this;
         this.request = { };
         // console.log(this.request);
 
-        this.addRequest = function(info) {
+        this.addRequest = function( info ) {
           // this.request.createdOn = Date.now();
-          info.requests.push(this.request);
+          info.requests.push( this.request );
           this.request = { };
         };
-
-        $http.get('https://dolmen.firebaseio.com/' + '.json')
-        .then(function(response){
-          // console.log(response.data);
+        $http.get( 'https://dolmen.firebaseio.com/' + '.json' )
+        .then( function( response ){
+          console.log(response.data);
           show.requests = response.data;
-
         });
-
       });
-
-
     })();
