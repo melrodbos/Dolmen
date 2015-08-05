bcf47cbc4fffefcc6dc2a79b4ae4369e9925d220
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-08-04 22:40:10 -0400

    commenting out unused code

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -1,19 +1,19 @@
-;(function() {
-  'use strict';
-
-  angular
-    .module('dolmen')
-    .config(config);
-
-  /** @ngInject */
-  function config($logProvider, toastr) {
-    // Enable log
-    $logProvider.debugEnabled(true);
-
-    // Set options third-party lib
-    toastr.options.timeOut = 3000;
-    toastr.options.positionClass = 'toast-top-right';
-    toastr.options.preventDuplicates = true;
-    toastr.options.progressBar = true;
-  }
-})();
+// ;(function() {
+//   'use strict';
+//
+//   angular
+//     .module('dolmen')
+//     .config(config);
+//
+//   /** @ngInject */
+//   function config($logProvider, toastr) {
+//     // Enable log
+//     $logProvider.debugEnabled(true);
+//
+//     // Set options third-party lib
+//     toastr.options.timeOut = 3000;
+//     toastr.options.positionClass = 'toast-top-right';
+//     toastr.options.preventDuplicates = true;
+//     toastr.options.progressBar = true;
+//   }
+// })();
