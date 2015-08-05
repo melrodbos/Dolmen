0668035c87a1ca6f28ef179b1d154cebbe757056
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-08-04 22:42:52 -0400

    moved module dependencies to, index.routes

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -1,19 +1,18 @@
-(function() {
-  'use strict';
 
-  angular
-    .module('dolmen', [
-      'ngAnimate',
-      'ngCookies',
-      'ngTouch',
-      'ngSanitize',
-      'restangular',
-      'ui.router',
-      'ui.bootstrap',
-      'firebase',
-      'xeditable',
-      // 'dolmen.services',
-    ]
-    //
-  ).constant( 'FBDolmen', 'https://dolmen.firebaseio.com/owners');
-})();
+// (function() {
+//   'use strict';
+//
+//   angular.module('dolmen', [
+//       'ngAnimate',
+//       'ngCookies',
+//       'ngTouch',
+//       'ngSanitize',
+//       'restangular',
+//       'ui.router',
+//       'ui.bootstrap',
+//       'firebase',
+//       'xeditable',
+//     ]).controller( '' )
+//
+//
+// })();
