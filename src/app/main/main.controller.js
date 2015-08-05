e3b696fc1dc7b5d1a18945ac391851c4cf5bd184
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-08-04 22:47:48 -0400

    Login controller redefined

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,39 +1,71 @@
-(function() {
+(function(){
   'use strict';
 
-  angular
-  .module('dolmen')
-    .controller('MainController', MainController);
+  var app = angular.module( 'dolmen' );
+  app.controller( 'MainController', function( FBDolmen, $firebaseArray, $firebaseObject, Auth ){
 
-  /** @ngInject */
-  function MainController($timeout, webDevTec, toastr) {
-    var vm = this;
+    var self = this;
+    var ownerInfo = new Firebase( FBDolmen + '/owners' );
 
-    vm.awesomeThings = [];
-    vm.classAnimation = '';
-    vm.creationDate = 1436460613764;
-    vm.showToastr = showToastr;
+    this.obj = $firebaseArray( ownerInfo );
+    console.log( this.obj );
 
-    activate();
+    this.ownerArray = {};
 
-    function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
-    }
+    this.googleLogin = Auth.googleLogin;
 
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
-    }
+    Auth.onAuth( function( owner ){
+      self.owner = owner;
+      if ( owner === null ){
+        console.log( 'null' );
+      } else {
+        console.log( 'owner' );
+      }
+    });
+  });
+})();//End of IIFE
 
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
 
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-    }
-  }
-})();
+
+//----------------------------------------------
+// //NOTE: Gulp_angular Default MainController:
+
+// (function() {
+//   'use strict';
+//
+//   angular
+//   .module('dolmen')
+//     .controller('MainController', MainController);
+//
+//   /** @ngInject */
+//   function MainController($timeout, webDevTec, toastr) {
+//     var vm = this;
+//
+//     vm.awesomeThings = [];
+//     vm.classAnimation = '';
+//     vm.creationDate = 1436460613764;
+//     vm.showToastr = showToastr;
+//
+//     activate();
+//
+//     function activate() {
+//       getWebDevTec();
+//       $timeout(function() {
+//         vm.classAnimation = 'rubberBand';
+//       }, 4000);
+//     }
+//
+//     function showToastr() {
+//       toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
+//       vm.classAnimation = '';
+//     }
+//
+//     function getWebDevTec() {
+//       vm.awesomeThings = webDevTec.getTec();
+//
+//       angular.forEach(vm.awesomeThings, function(awesomeThing) {
+//         awesomeThing.rank = Math.random();
+//       });
+//     }
+//   }
+// })();
