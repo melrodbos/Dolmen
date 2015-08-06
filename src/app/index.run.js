aa1132da6ae7ee37ff414846b56230a9e86f90df
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-08-06 12:54:33 -0400

    updated fn parameters and module dependencies

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -1,22 +1,28 @@
-;(function() {
+;( function() {
   'use strict';
 
 
-  var app = angular.module('dolmen')
+  var app = angular.module( 'dolmen' )
     .run(runBlock);
 
   /** @ngInject */
-  function runBlock($log) {
+  function runBlock( $log ) {
 
-    $log.debug('runBlock end');
+    $log.debug( 'runBlock end' );
   }
 //add .run scripts here:
-app.run(['$rootScope', '$location', function($rootScope, $location)
+app.run(['$rootScope', '$sessionStorage', '$state', function( $rootScope, $sessionStorage, $state )
 {
-  $rootScope.$on('$routeChangeError', function(event, next, previous, error) {
-    if (error === "AUTH_REQUIRED") {
-      $location.path("/");
+  $rootScope.$on( '$stateChangeStart', function( events, to ) {
+    if( to.data.needsAuth )
+    {
+      
     }
+    // if ( to.data.needsAuth && $sessionStorage.ownerSession ) {
+    //   var movingOn = true;
+    // } else {
+    //   $state.go( 'home' );
+    // }
   });
 }]);
 
