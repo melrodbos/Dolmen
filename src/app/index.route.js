7acb62523a8b4ffb3bed1ef5e75cbb0e8cabd3fb
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-08-06 12:51:40 -0400

    added module and tool dependencies to main module

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -5,48 +5,41 @@
    'ui.router',
    'firebase',
    'ui.bootstrap',
-   'xeditable' ])
+   'xeditable',
+   'ngStorage',
+   'dolmen.services',
+   'dolmen.dashboard',
+   'dolmen.requestForm'
+  ])
   .config( function ( $stateProvider, $urlRouterProvider){
     $stateProvider
       .state('home', {
         url: '/',
         templateUrl: 'app/main/main.html',
-        controller: 'MainController',
-        controllerAs: 'MainCtrl'
+        data: {
+          needsAuth: false
+        }
       })
-
       .state('dashboard', {
         url: '/dashboard',
-        templateUrl: 'app/po_dash/po_dash.html'
-        // controller: 'DashboardController',
-        // controllerAs: 'dashboard'
+        templateUrl: 'app/po_dash/po_dash.html',
+        data: {
+          needsAuth: true
+        }
       })
-
-      .state('pending', {
-        url: '/pending',
-        templateUrl: 'app/po_dash/pending_requests.html',
-        controller: 'PendingController',
-        controllerAs: 'pending'
+      .state( 'active', {
+        url: '/active',
+        templateUrl: 'app/po_dash/po_active_details.html',
+        data: {
+          needsAuth: true
+        }
       })
-
       .state('po_form', {
         url: '/requests',
-        templateUrl: 'app/po_dash/po_form.html'
-        // controller: 'RequestController',
-        // controllerAs: 'request',
-      })
-
-      .state('po_active', {
-        url: '/active',
-        templateUrl: 'app/po_dash/po_active_details.html'
-      })
-
-      .state('properties', {
-        url: '/properties',
-        templateUrl: 'app/po_dash/po_properties.html'
+        templateUrl: 'app/po_dash/po_form.html',
+        data: {
+          needsAuth: true
+        }
       });
-
-      $urlRouterProvider.otherwise('/');
-
-}); //End of routeConfig
+    }); //End of routeConfig
 })(); //End of IIFE
