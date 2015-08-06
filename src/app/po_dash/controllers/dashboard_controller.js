997a8731397a22c99a06f0e105df8ec9279802f4
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-08-06 13:08:14 -0400

    created dashboard controller w req filter starting point, can filter active reqs

--- src/app/po_dash/controllers/dashboard_controller.js
+++ src/app/po_dash/controllers/dashboard_controller.js
@@ -1,89 +1,23 @@
-// (function(){
-//   'use strict';
-// //FIXME: how do I make this work!!!:
-//   var app = angular.module( 'dolmen' );
-//   app.filter( 'requestfilter', function ( FBDolmen, $location ){
-//     return function ( input ) {
-//       var filtered = { };
-//       angular.forEach( input, function ( request, id ){
-//         var path = $location.path(/* path here*/);
-//         if ( status === '/pending' ) {
-//           if ( !request.completed ) {
-//             filtered[ id ] = request;
-//           }
-//         } else if ( status === '/active' ) {
-//           if ( !request.completed ) {
-//             // then location.path('/')
-//             filtered[ id ] = request;
-//           }
-//         } else if ( status === '/completed' ) {
-//           if ( request.completed ) {
-//             filtered[ id ] = request;
-//           }
-//         } else {
-//           filtered[ id ] = request;
-//         }
-//       });
-//       return filtered;
-//     };
-//   });
-//
-//
-//
-//
-//
-//
-// })();
+(function(){
+  'use strict';
 
+  angular.module( 'dolmen.dashboard', [ 'dolmen.services', 'ngStorage' ] )
+  .controller( 'DashboardController', [ '$rootScope', '$scope', 'Maintenance', '$sessionStorage', '$timeout',
+  function( $rootScope, $scope, Maintenance, $sessionStorage, $timeout ){
+    var self = this;
+    self.requests = [];
+    Maintenance.getRequests( $sessionStorage.ownerSession.google.id );
+    $rootScope.$on( 'requestsRetrieved', function(){
+      $timeout( function(){
+        self.requests = Maintenance.requests;
+        self.requests = $.map(self.requests, function(value, index) {
+            value.dbId = index;
+            return [value];
+        });
+      });
+    });
 
+    $scope.filterActive = function( req ){ return req.status === 'Active'; };
+  }]);
 
-
-
-
-
-
-
-
-
-
-
-
-
-// (function(){
-//   'use strict';
-//
-//   angular.module( 'dolmen', function( $scope, $location, ownerService, FBDolmen, $firebaseAuth ){
-//
-//     $scope.owner = ownerService.getLoggedInOwner();
-//
-//     $scope.newThreadTitle = '';
-//
-//     $scope.threads = threadService.getAllThreads();
-//
-//     $scope.threads.$loaded().then( function(){
-//       // console.log( $scope.threads );
-//     });
-//
-//     $scope.addThread = function(){
-//       if( !$scope.newThreadTitle ){
-//         // FIXME: Preventing Event Propagation in AngularJs.
-//         return false;
-//       }
-//       var newThread = {
-//         username: $scope.owner.name,
-//       };
-//
-//       $scope.threads.$add( newThread );
-//
-//       $scope.newThreadTitle = '';
-//     };
-//
-//     $scope.logout = function(){
-//       var ref = new Firebase( FBDolmen.url );
-//       var authObj = $firebaseAuth( ref );
-//       authObj.$unauth();
-//       $location.path( '/' );
-//     };
-//   });
-//
-// });
+})();
