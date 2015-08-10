8191a924526eca0432d0a1445955ab541ec4aebe
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-08-09 21:50:27 -0400

    attempt at date picker default date settingup

--- src/app/po_dash/controllers/request_form.js
+++ src/app/po_dash/controllers/request_form.js
@@ -1,24 +1,25 @@
-(function(){
+(function() {
   'use strict';
 
-  angular.module( 'dolmen.requestForm',[ 'dolmen.services', 'ngStorage' ] )
-  .controller( 'RequestController', [ '$scope', 'Maintenance',
-    '$sessionStorage', '$state',
-    function( $scope, Maintenance, $sessionStorage, $state ){
-      var self = this;
-      self.submit = function() {
-        Maintenance.addRequest({
-          date: self.date,
-          // dateComplete: 'tbd',
-          category: self.category,
-          address: self.address,
-          phone: self.phone,
-          description: self.description,
-          instructions: self.instructions,
-          status: self.status,
-          oid: $sessionStorage.ownerSession.google.id
-        });
-        $state.go( 'dashboard');
+  angular.module('dolmen.requestForm', ['dolmen.services', 'ngStorage'])
+    .controller('RequestController', ['$scope', 'Maintenance',
+      '$sessionStorage', '$state',
+      function($scope, Maintenance, $sessionStorage, $state) {
+        var self = this;
+        self.submit = function() {
+          Maintenance.addRequest({
+            date: self.date,
+            // dateComplete: 'tbd',
+            category: self.category,
+            address: self.address,
+            phone: self.phone,
+            description: self.description,
+            instructions: self.instructions,
+            status: self.status,
+            oid: $sessionStorage.ownerSession.google.id
+          });
+
+          $state.go('dashboard');
 
           self.date = '';
           self.category = '';
@@ -27,7 +28,6 @@
           self.description = '';
           self.instructions = '';
           self.status = '';
-      };
-
-  }]);
+        };
+    }]);
 })();
